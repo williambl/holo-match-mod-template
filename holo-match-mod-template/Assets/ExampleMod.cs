1fb99dbbd519e0090519044cf6b43ae783e9df4b
williambl <william@williambl.com>
2018-09-22 20:01:24 +0100

    Add ExampleMod class

---
+++ holo-match-mod-template/Assets/ExampleMod.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ExampleMod : HoloMod {
+
+    public string name = "ExampleMod";
+
+    public string version = "v0.1";
+
+    public override void RegisterWeapons(WeaponManager manager) {
+    }
+
+    public override void RegisterMaps(MapManager manager) {
+    }
+}
