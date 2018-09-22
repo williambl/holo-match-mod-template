3d3d457e2b298e6d9f829f4b5b78e459a6032e12
williambl <william@williambl.com>
2018-09-22 19:54:23 +0100

    Add CreateAssetBundles script from unity docs

--- holo-match-mod-template/Assets/Editor/CreateAssetBundles.cs
+++ holo-match-mod-template/Assets/Editor/CreateAssetBundles.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.IO;
+using UnityEditor;
 
-public class CreateAssetBundles : MonoBehaviour {
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if(!Directory.Exists(assetBundleDirectory))
+            Directory.CreateDirectory(assetBundleDirectory);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
 }
