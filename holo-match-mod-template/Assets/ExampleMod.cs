da5656a92f15850698870d751239da8c28d80ce9
williambl <william@williambl.com>
2018-09-24 22:02:25 +0100

    Add some debugging code during weapon registration

--- holo-match-mod-template/Assets/ExampleMod.cs
+++ holo-match-mod-template/Assets/ExampleMod.cs
@@ -7,6 +7,7 @@ public class ExampleMod : HoloMod {
     public string version = "v0.1";
 
     public override void RegisterWeapons(WeaponManager manager) {
+        Debug.Log(assetBundles[0].LoadAsset<GameObject>("TestAsset").name);
     }
 
     public override void RegisterMaps(MapManager manager) {
