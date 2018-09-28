e1418d61b1628c453ef61da6defdef8c33154268
williambl <william@williambl.com>
2018-09-28 20:15:11 +0100

    Made a proper build script

--- build-mod-dll.sh
+++ build-mod-dll.sh
@@ -1,1 +1,9 @@
-mcs holo-match-mod-template/Assets/ExampleMod.cs -target:library -r:/home/william/dev/unity_projects/holo-match/bin/prealpha-0.07/prealpha-0.07_Data/Managed/Assembly-CSharp.dll -r:/home/william/dev/unity_projects/holo-match/bin/prealpha-0.07/prealpha-0.07_Data/Managed/UnityEngine.dll -r:/home/william/dev/unity_projects/holo-match/bin/prealpha-0.07/prealpha-0.07_Data/Managed/UnityEngine.Networking.dll -r:/home/william/dev/unity_projects/holo-match/bin/prealpha-0.07/prealpha-0.07_Data/Managed/UnityEngine.CoreModule.dll -r:/home/william/dev/unity_projects/holo-match/bin/prealpha-0.07/prealpha-0.07_Data/Managed/UnityEngine.AssetBundleModule.dll
+if [ ! -d $1 ]
+then
+    echo Path to holomatch_data:
+    read data_dir
+else
+    data_dir=$1
+fi
+
+mcs `find ./holo-match-mod-template/Assets/Scripts/ -name *.cs -print | tr '\n' ' '` -out:ExampleMod.dll -target:library -r:$data_dir/Managed/Assembly-CSharp.dll -r:$data_dir/Managed/UnityEngine.dll -r:$data_dir/Managed/UnityEngine.Networking.dll -r:$data_dir/Managed/UnityEngine.CoreModule.dll -r:$data_dir/Managed/UnityEngine.AssetBundleModule.dll
