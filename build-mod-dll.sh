5006f0ad6daa892b3b433e7bcb7144c16b761f50
williambl <william@williambl.com>
2018-09-29 22:39:50 +0100

    Hardcode less in build script

--- build-mod-dll.sh
+++ build-mod-dll.sh
@@ -1,3 +1,6 @@
+mod_name=ExampleMod
+mod_dir=holo-match-mod-template
+
 if [ ! -d $1 ]
 then
     echo Path to holomatch_data:
@@ -6,4 +9,4 @@ else
     data_dir=$1
 fi
 
-mcs `find ./holo-match-mod-template/Assets/Scripts/ -name *.cs -print | tr '\n' ' '` -out:ExampleMod.dll -target:library -r:$data_dir/Managed/Assembly-CSharp.dll -r:$data_dir/Managed/UnityEngine.dll -r:$data_dir/Managed/UnityEngine.Networking.dll -r:$data_dir/Managed/UnityEngine.CoreModule.dll -r:$data_dir/Managed/UnityEngine.AssetBundleModule.dll
+mcs `find ./$mod_dir/Assets/Scripts/ -name *.cs -print | tr '\n' ' '` -out:$mod_name.dll -target:library -r:$data_dir/Managed/Assembly-CSharp.dll -r:$data_dir/Managed/UnityEngine.dll -r:$data_dir/Managed/UnityEngine.Networking.dll -r:$data_dir/Managed/UnityEngine.CoreModule.dll -r:$data_dir/Managed/UnityEngine.AssetBundleModule.dll
