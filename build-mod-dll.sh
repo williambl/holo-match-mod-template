e99412982f08399ce6e0a1b35cbe45c00ace4927
williambl <william@williambl.com>
2018-09-29 22:46:59 +0100

    build-mod-dll.sh now creates directory for build and copies asset bundles into it

--- build-mod-dll.sh
+++ build-mod-dll.sh
@@ -9,4 +9,22 @@ else
     data_dir=$1
 fi
 
+if [ -d $mod_name ]
+then
+    echo Please remove $mod_name directory. I won\'t do it in case i accidentally remove / or something.
+    exit
+else
+    mkdir $mod_name
+    cd $mod_name
+fi
+
+echo Copying AssetBundles...
+find ./$mod_dir/Assets/AssetBundles/ -name *.assetbundle -exec cp {} . \;
+
+echo AssetBundles Copied.
+
+echo Building C# Scripts...
+
 mcs `find ./$mod_dir/Assets/Scripts/ -name *.cs -print | tr '\n' ' '` -out:$mod_name.dll -target:library -r:$data_dir/Managed/Assembly-CSharp.dll -r:$data_dir/Managed/UnityEngine.dll -r:$data_dir/Managed/UnityEngine.Networking.dll -r:$data_dir/Managed/UnityEngine.CoreModule.dll -r:$data_dir/Managed/UnityEngine.AssetBundleModule.dll
+
+echo C# Scripts Built!
